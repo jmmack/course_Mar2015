6e908ed29e6e88bd37603e62745fb0611623bd3b
mmacklai <jean.macklaim@gmail.com>
2015-03-18 11:10:14 -0400

    Example code to run DESeq2 from matrix of counts
    Example code to run DESeq2 from matrix of counts

---
+++ deseq2example.R
@@ -0,0 +1,26 @@
+# Basic DESeq2 commands for a read counts table
+# Using the selex dataset from ALDEx2
+
+library(DESeq2)
+library(ALDEx2)
+
+#load selex datafame
+data("selex")
+
+#format into matrix of read counts
+countsTable <- data.matrix(selex,rownames.force=NA)
+#make table of conditions in same order as columns of counts table
+#i.e. the first 7 samples are "N" condtion and the next 7 samples are "S" condition
+coldata <- data.frame(condition=factor(c(rep("N",7), rep("S", 7))))
+
+#DESeq2 formats data from a matrix of read counts
+dds <- DESeqDataSetFromMatrix(countData=countsTable, colData=coldata, design = ~condition)
+
+ddsoutput <- DESeq(dds, fitType="local")
+
+res2 <- results(ddsoutput)
+
+
+plotMA(ddsoutput)
+
+
